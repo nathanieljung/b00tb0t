74f42c3bda6d9ef4d8a868b8da72402aed8e347b
Nate Jung <nathanieljung1@gmail.com>
2020-12-01 02:35:24 +0000

    Made grive command work (in theory).

--- plugins/gitoperations.py
+++ plugins/gitoperations.py
@@ -1,6 +1,7 @@
 #imports
 import discord, random
 from discord.ext import commands
+import subprocess
 
 class GitOperations(commands.Cog):
     def __init(self, bot):
@@ -10,6 +11,12 @@ class GitOperations(commands.Cog):
     @commands.command()
     async def grive(self, ctx):
         await ctx.send('s\ny\nn\nc\ni\nn\ng\n\nl\ni\nb\ns')
+        output = subprocess.run('../grive.sh',check=TRUE, stdout=PIPE).stdout
+        if 'ERROR: git not clean. Can\'t pull' in output:
+            await ctx.send('GRIVE FAILED')
+        else:
+            await ctx.send('GRIVE SUCCESS')
+
 #cog setup
 def setup(bot):
     bot.add_cog(GitOperations(bot))
