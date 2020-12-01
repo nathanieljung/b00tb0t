bcaac1ff9cc61d7e764d0a568880704412751bd2
Nate <kipssnicks22@gmail.com>
2020-11-30 22:20:20 -0500

    Added LFG command and general picture commands python file.

---
+++ plugins/picturecommands.py
@@ -0,0 +1,16 @@
+#imports
+import discord, random
+from discord.ext import commands
+
+class HangoutsCommands(commands.Cog):
+    def __init(self, bot):
+        self.bot = bot
+
+    #Commands
+    @commands.command()
+    async def LFG(self, ctx):
+        '''replies with 'LET'S FUCKING GO!' pic '''
+        await ctx.send('https://media.discordapp.net/attachments/775772474621165588/778466913872379924/EnEJNYbXcAUsBT8.png?width=249&height=375')
+
+def setup(bot):
+    bot.add_cog(HangoutsCommands(bot))
