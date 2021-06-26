f49258c9f92b2038eef302573c78dfbf473bd00f
Nate Jung <nathanieljung@protonmail.com>
2021-06-25 23:34:59 -0400

    Testing slash commands

--- start.py
+++ start.py
@@ -3,6 +3,8 @@ import discord
 
 from discord.ext import commands
 
+from discord_slash import SlashCommand, SlashContext
+
 from io import FileIO
 import json
 
@@ -82,7 +84,7 @@ def getPluginList():
         else: continue
     return returnPlugins
 
-@bot.command()
+@slash.slash(name="listplugins", description="Testing slash commands with this one.")
 async def listplugins(ctx):
     '''This function gets all available plugins'''
     stritem ='```\n'
