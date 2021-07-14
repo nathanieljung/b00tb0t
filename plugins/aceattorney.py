dfcf0630548f82e799ee005bef915fc0c8ff9d7f
Nate Jung <nathanieljung@protonmail.com>
2021-07-14 08:42:51 -0400

    Testing message history

--- plugins/aceattorney.py
+++ plugins/aceattorney.py
@@ -9,7 +9,10 @@ class AceAttorney(commands.Cog):
     #Commands
     @commands.command()
     async def lastcommands(ctx, num_comments):
-        test = ""
+        to_send = ''
+        async for message in ctx.channel.history(limit=int(num_comments)):
+            to_send += message.author.name + ': ' + message.content + '\n'
+        await ctx.send(to_send)
 
 def setup(bot):
     bot.add_cog(AceAttorney(bot))
