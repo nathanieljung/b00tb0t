810e3f9833c26c67e1ddc8885112cac861b7a231
Nate Jung <nathanieljung@protonmail.com>
2021-07-14 17:19:10 -0400

    I should know better than this, but this should allow the user to generate an Ace Attorney video.

--- plugins/aceattorney.py
+++ plugins/aceattorney.py
@@ -1,6 +1,21 @@
 #imports
-import discord, random
-from discord.ext import commands
+from discord.ext.commands.core import command
+
+from discord.ext import commands, Context as context
+
+from collections import Counter
+
+import anim, os, discord
+
+class DiscordAuthor:
+        def __init__(self, name):
+            self.name = name
+
+class DiscordComment:
+    def __init__(self, username: str, text, str, score: int = 0):
+        self.author = DiscordAuthor(username)
+        self.body = text
+        self.score = score
 
 class AceAttorney(commands.Cog):
     def __init(self, bot):
@@ -15,6 +30,33 @@ class AceAttorney(commands.Cog):
         async for message in history:
             to_send += message.author.name + ': ' + message.content + '\n'
         await ctx.send(to_send)
+
+    @commands.command()
+    async def acecourt(self, ctx: context, num_comments: int):
+        unames = []
+        origcomments = []
+        history = ctx.channel.history(limit=num_comments + 1)
+        await history.next()
+        async for message in history:
+            unames.append(message.author.name)
+            origcomments.append(message.content)
+        cnt = Counter(unames)
+        most_common_pair = cnt.most_common(len(cnt))
+        most_common = []
+        for a, b in most_common_pair:
+            most_common.append(a)
+        
+        characters = anim.get_characters(most_common=most_common)
+        comments = []
+        for uname, comment in zip(unames, origcomments):
+            comments.append(DiscordComment(uname, comment))
+        comments.reverse()
+        anim.comments_to_scene(comments, characters, output_filename="ace.mp4")
+        fileSize = os.path.getsize("ace.mp4")
+        if fileSize < ctx.channel.guild.filesize_limit:
+            await ctx.send(content="", file=discord.File("ace.mp4"))
+        else:
+            ctx.send("The resulting filesize is too big to send.")
 
 def setup(bot):
     bot.add_cog(AceAttorney(bot))
