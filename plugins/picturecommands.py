a4bd891b6184c73238babdfa1a95970b8f1058b2
Nathaniel Jung <nathanieljung1@gmail.com>
2022-09-14 08:08:29 -0400

    Added delet command

--- plugins/picturecommands.py
+++ plugins/picturecommands.py
@@ -1,5 +1,5 @@
 #imports
-import discord, random
+import discord, random, os
 from discord.ext import commands
 
 class PictureCommands(commands.Cog):
@@ -12,5 +12,10 @@ class PictureCommands(commands.Cog):
         '''replies with 'LET'S FUCKING GO!' pic '''
         await ctx.send('https://media.discordapp.net/attachments/775772474621165588/778466913872379924/EnEJNYbXcAUsBT8.png?width=249&height=375')
 
+    @commands.command()
+    async def delet(self, ctx):
+        '''replies with a random delet image'''
+        picture = discord.File('./plugins/delet/{}'.format(random.choice(os.listdir('./plugins/delet'))))
+        await ctx.send(file=picture)
 def setup(bot):
     bot.add_cog(PictureCommands(bot))
